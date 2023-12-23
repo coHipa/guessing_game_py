a212b8fa2cd74bd9c94ef83feb16faf892769f31
coHipa <96304612+coHipa@users.noreply.github.com>
2023-12-23 21:47:59 +0100

    Minor changes, added names, added class methods for beeing alive (readebility in conditions)

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ import random
 
 
 class Player:
-    def __init__(self, hp, dmg):
+    def __init__(self, name, hp, dmg):
+        self.name = name
         self.hp = hp
         self.dmg = dmg
+
+    def is_alive(self):
+        return self.hp > 0
 
 
 class Enemy:
     def __init__(self, hp, dmg):
         self.hp = hp
         self.dmg = dmg
+        self.name = random.choice(["Vergil", "Malenia", "C'Thun", "Baal", "Wizpig", "The Nameless"])
+
+    def is_alive(self):
+        return self.hp > 0
 
 
 def main():
+    player_name = input("Enter your name: ")
+    player = Player(player_name, hp=100, dmg=10)
     print("Guess through hell\n")
-    print("You wake up in hell and have to guess your way out.")
+    print("{0} you wake up in hell and have to guess your way out.".format(player.name))
 
-    player = Player(hp=100, dmg=10)
-    while player.hp > 0:
+    while player.is_alive():
         enemy_count = random.randint(1, 5)
         boss_enemy = Enemy(hp=100, dmg=20)
 
@@ -32,20 +41,20 @@ def main():
 def attack_little(player, enemy):
     secret_number = random.randint(1, 10)
 
-    player_guess = int(input("Attack that little enemy (1-10) "))
+    player_guess = int(input("{player.name}, fast attack that little enemy (1-10) "))
     if player_guess == secret_number:
         enemy.hp -= player.dmg
         print("You killed that guy.\n")
     else:
         player.hp -= enemy.dmg
-        print("Your guess was wrong, you're hit {0} HP left".format(player.hp))
+        print("Your guess was wrong, you're hit {player.hp} HP left")
 
 
 def attack_boss(player, enemy):
     secret_number = random.randint(1, 50)
 
-    while enemy.hp > 0 and player.hp > 0:
-        player_guess = int(input("You have to fight a boss (1-50) "))
+    while enemy.is_alive() and player.is_alive():
+        player_guess = int(input("You have to fight against {enemy.name} (1-50) "))
 
         if player_guess > secret_number:
             player.hp -= enemy.dmg
@@ -55,7 +64,7 @@ def attack_boss(player, enemy):
             print("Your guess was to low, your HP: {0}\n".format(player.hp))
         else:
             enemy.hp -= player.dmg
-            print("You guessed right and hit the boss, remaining HP: {0}".format(enemy.hp))
+            print("You guessed right and hit the boss, remaining HP: {enemy.hp}")
             secret_number = random.randint(1, 50)
 
 
