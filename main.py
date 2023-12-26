0ed0157ae1ee68a248edb4da773566a77aed5a3b
coHipa <96304612+coHipa@users.noreply.github.com>
2023-12-26 21:00:21 +0100

    Fixed print format, added crit logic.

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ class Player:
         self.hp = hp
         self.dmg = dmg
         self.correct_guesses = 0
-        self.critical_dmg = 0
+        self.hits_to_crit = 4
+        self.critical_dmg = 1
 
     def is_alive(self):
         return self.hp > 0
     
     def update_dmg(self):
-        self.dmg = 10 + self.critical_dmg
+        self.dmg + self.critical_dmg
     
     def increase_critical_dmg(self):
         self.critical_dmg = int(self.dmg * (random.randint(20, 50) / 100))
@@ -30,51 +31,60 @@ class Enemy:
 
 
 def main():
-    HITS_TO_CRIT = 4
     player_name = input("Enter your name: ")
     player = Player(player_name)
     print("Guess through hell\n")
-    print("{player.name} you wake up in hell and have to guess your way out.")
+    print(f"{player.name} you wake up in hell and have to guess your way out.")
 
     while player.is_alive():
         enemy_count = random.randint(1, 5)
         boss_enemy = Enemy(hp=100, dmg=20)
 
         for i in range(enemy_count):
-            little_enemy = Enemy(hp=10, dmg=5)
-            attack_little(player, little_enemy)
+            minor_enemy = Enemy(hp=15, dmg=5)
+            attack_minor(player, minor_enemy)
 
         attack_boss(player, boss_enemy)
 
 
-def attack_little(player, enemy):
+def attack_minor(player, enemy):
     secret_number = random.randint(1, 10)
 
-    print("{player.name}, attack this little enemy, fast (1-10)")
+    print(f"{player.name}, attack this little enemy, fast (1-10)")
     if valid_guess() == secret_number:
+        critical_logic(player)
         enemy.hp -= player.dmg
         print("You killed that guy.\n")
     else:
         player.hp -= enemy.dmg
-        print("Your guess was wrong, you're hit {player.hp} HP left")
+        print(f"Your guess was wrong, you're hit {player.hp} HP left")
 
 
 def attack_boss(player, enemy):
     secret_number = random.randint(1, 50)
+    print(f"You have to fight against {enemy.name} (1-50)\n")
 
     while enemy.is_alive() and player.is_alive():
-        print("You have to fight against {enemy.name} (1-50)\n")
 
         if valid_guess() > secret_number:
             player.hp -= enemy.dmg
-            print("Your guess was to high, your HP: {player.hp}\n")
+            print(f"Your guess was to high, your HP: {player.hp}\n")
         elif valid_guess() < secret_number:
             player.hp -= enemy.dmg
-            print("Your guess was to low, your HP: {player.hp}\n")
+            print(f"Your guess was to low, your HP: {player.hp}\n")
         else:
+            critical_logic(player)
             enemy.hp -= player.dmg
-            print("You guessed right and hit the boss, remaining HP: {enemy.hp}")
+            print(f"You guessed right and hit the boss, remaining HP: {enemy.hp}")
             secret_number = random.randint(1, 50)
+
+
+def critical_logic(player):
+    player.correct_guesses += 1
+    if player.correct_guesses == player.hits_to_crit:
+        print("Your hit is a crit")
+        player.increase_critical_dmg()
+        player.correct_guesses = 0
 
 
 def valid_guess():
